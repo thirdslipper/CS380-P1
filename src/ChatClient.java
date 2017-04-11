ce5809a08b91d22593717aa3a956e8997a6b66e8
Colin Koo <chkoo@cpp.edu>
2017-04-11 00:02:12 -0700

    Listening done, inputs done, final submission

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,3 +1,8 @@
+/**
+ * Author: Colin Koo
+ * Professor: Davarpanah
+ * Description: This program emulates a chat client to a server.
+ */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,41 +14,54 @@ import java.net.UnknownHostException;
 import java.util.Scanner;
 
 
-public class ChatClient extends Thread implements Runnable{
+public class ChatClient{
+	/**
+	 * This method connects to the public codebank.xyz server at port 38001, representing our class'.
+	 * It writes to the server stream through a Socket, and a PrintWriter, reading from the ServerSocket stream
+	 * with the same Socket, and BufferedReader.
+	 * The first input the user makes represents their user name, and the program continues depending on 
+	 * whether or not the name is available.  If the user name is available, every message the user types
+	 * 
+	 * @param args
+	 * @throws UnknownHostException
+	 * @throws IOException
+	 */
 	public static void main(String[] args) throws UnknownHostException, IOException{
 
-
 		try (Socket socket = new Socket("codebank.xyz", 38001)){
-			System.out.println("Connected to: " + socket.getInetAddress() + ": " + socket.getPort());
-			
+			System.out.println("Connected to: " + socket.getInetAddress() + ":" + socket.getPort());
+
 			PrintWriter pw = new PrintWriter(socket.getOutputStream());
 			BufferedReader br = new BufferedReader
 					(new InputStreamReader(socket.getInputStream(), "UTF-8"));	
-			
-			
+
+
 			Scanner kb = new Scanner(System.in);
 			System.out.print("Enter a user name: ");
-			String username = kb.next();
-			
-			pw.println(username);	//write to output
+			String input = kb.nextLine();
+			pw.println(input);
 
-			String input = "";
-			if (br.readLine().equals("Name in use.")){
-				System.out.println(br.readLine());
-			}
-/*			else{
-				while (!input.toLowerCase().equals("exit")){
+			if (!(br.ready() && br.readLine().equals("Name in use."))){
+				new Listener(socket, br).start();
+//				while (!input.toLowerCase().equals("exit")){
+				while (true){
 					pw.flush();
-					
 					input = kb.nextLine();
-					pw.write(input);
-					System.out.println(br.readLine());
+					pw.println(input);
 				}
-			}*/
+			}
+			kb.close();
 		}
 	}
-	
-/*	public class Listener extends Thread implements Runnable{
+
+	/**
+	 * This nested class is a thread that runs conditionally in the main method of ChatClient.
+	 * If the user enters a free user name, then the Listener thread starts, waiting for either a client
+	 * to submit responses to the server to display.
+	 * @author Colin
+	 *
+	 */
+	public static class Listener extends Thread implements Runnable{
 		Socket socket;
 		BufferedReader br;
 		public Listener(Socket socket, BufferedReader br){
@@ -51,8 +69,17 @@ public class ChatClient extends Thread implements Runnable{
 			this.br = br;
 		}
 		public void run(){
-			
+			while (true){
+				try {
+					if (br.ready()){
+						System.out.println(br.readLine());
+					}
+				} catch (Exception E){
+					E.printStackTrace();
+				}
+			}
 		}
-	}*/
+	}
 }
+
 
